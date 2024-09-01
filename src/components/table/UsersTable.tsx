c8ff0a24ddd8da98266e1bac358502e4f4dfadd7
Anatolii_Filipenko <anatoliy.fil033@gmail.com>
2024-09-01 12:26:29 +0300

    added loading in UsersTable

--- src/components/table/UsersTable.tsx
+++ src/components/table/UsersTable.tsx
@@ -10,12 +10,12 @@ import "primeicons/primeicons.css";
 
 const UsersTable = () => {
     const users = useSelector((state: AppRootStateType) => state.users.users);
+    const loading = useSelector((state: AppRootStateType) => state.users.isLoading);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         dispatch(getUsers());
     }, []);
-
 
     return (
         <DataTable
@@ -23,6 +23,7 @@ const UsersTable = () => {
             filterDisplay="row"
             paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
             emptyMessage="No data found"
+            loading={loading}
         >
             <Column field="name" header="Name" filterField="name" sortable filter filterPlaceholder="Search"/>
             <Column field="username" sortable header="Username" filterField="username" filter
